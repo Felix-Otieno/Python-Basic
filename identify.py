38a873dad650e9476cf51b36e44e8f25ba98bef1
Felix-Otieno <omondifelixotieno2@gmail.com>
2024-10-19 08:58:31 +0300

    variables

---
+++ identify.py
@@ -0,0 +1,5 @@
+n = 300
+m = n
+print(id(n)) # same memory address
+print(id(m)) # same memory address 
+print(n == m)
---
+++ name.py
@@ -0,0 +1,3 @@
+my_list =[10,20,20.5,'Python',100]
+# It will print only datatype of variable
+print(type(my_list).__name__) # list
